63a699bacd2f8b726a8ebfca57910c30e9b0f77a
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2018-10-29 16:31:43 -0500

    fix: use ResizeObserver for checking element resize change

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { render } from 'react-dom'
 import useComponentSize from './'
 
@@ -6,11 +6,13 @@ function App() {
   let ref = useRef(null)
   let { width, height } = useComponentSize(ref)
   let imgUrl = `https://via.placeholder.com/${width}x${height}`
+  const [fullWidth, setFullWidth] = useState(false)
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <pre>{JSON.stringify({ width, height })}</pre>
-      <div ref={ref} style={{ width: '50%', height: '50%' }}>
+      <button onClick={() => setFullWidth(!fullWidth)}>Toggle Size</button>
+      <div ref={ref} style={{ width: fullWidth ? '100%' : '50%', height: '50%' }}>
         <img src={imgUrl} width={width} height={height} />
       </div>
     </div>
