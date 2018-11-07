8156a211a3240cb343beb2308bb8da86371d8fd5
Niek Kruse <niekkruse70@gmail.com>
2018-11-08 00:14:23 +0100

    Add TS and flow typings

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,4 @@ interface ComponentSize {
   height: number
 }
 
-export default function useComponentSize(): ComponentSize
+export default function useComponentSize<T = any>(ref: React.RefObject<T>): ComponentSize
