f04a2f071cd7c67eaeb37851e96b8b49d03c563c
Jaehyeon Nam <unchai@protonmail.com>
2019-06-17 21:58:21 +0900

    transpiling to es5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict'
-let { useCallback, useState, useLayoutEffect } = require('react')
+var _require = require('react')
+var useCallback = _require.useCallback
+var useState = _require.useState
+var useLayoutEffect = _require.useLayoutEffect
 
 function getSize(el) {
   if (!el) {
@@ -16,11 +19,11 @@ function getSize(el) {
 }
 
 function useComponentSize(ref) {
-  let [ComponentSize, setComponentSize] = useState(
-    getSize(ref ? ref.current : {})
-  )
+  var _useState = useState(getSize(ref ? ref.current : {}))
+  var ComponentSize = _useState[0]
+  var setComponentSize = _useState[1]
 
-  const handleResize = useCallback(
+  var handleResize = useCallback(
     function handleResize() {
       if (ref.current) {
         setComponentSize(getSize(ref.current))
@@ -30,7 +33,7 @@ function useComponentSize(ref) {
   )
 
   useLayoutEffect(
-    () => {
+    function () {
       if (!ref.current) {
         return
       }
@@ -38,17 +41,17 @@ function useComponentSize(ref) {
       handleResize()
 
       if (typeof ResizeObserver === 'function') {
-        let resizeObserver = new ResizeObserver(() => handleResize())
+        var resizeObserver = new ResizeObserver(function () { handleResize() })
         resizeObserver.observe(ref.current)
 
-        return () => {
+        return function () {
           resizeObserver.disconnect(ref.current)
           resizeObserver = null
         }
       } else {
         window.addEventListener('resize', handleResize)
 
-        return () => {
+        return function () {
           window.removeEventListener('resize', handleResize)
         }
       }
