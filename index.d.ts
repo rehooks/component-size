1f86a1fb05611141f2122c5b8e0858e3d07097be
Omar Diab <me@omardiab.com>
2020-03-04 21:08:45 +0900

    constrain T in typescript

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,4 @@ interface ComponentSize {
   height: number
 }
 
-export default function useComponentSize<T = any>(ref: React.RefObject<T>): ComponentSize
+export default function useComponentSize<T extends Element = Element>(ref: React.RefObject<T>): ComponentSize
